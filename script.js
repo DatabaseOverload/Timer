6cae1d458d537a7aa61bd89dd848fd8739dca4d7
Niclas Wagener <niclas.wagener@gmail.com>
2023-05-16 19:28:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const countDownDate = new Date("May 18, 2023 12:00:00").getTime();
+const countDownDate = new Date("Jan 1, 2123 23:59:59").getTime();
 
 const x = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
 
-    const now = new Date().getTime();
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    const distance = countDownDate - now;
+  const demoElement = document.querySelector(".container .timer");
+  demoElement.innerHTML = `${days}D ${hours}H ${minutes}M ${seconds}S`;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    const demoElement = document.querySelector(".container .demo");
-    demoElement.innerHTML = `${days}D ${hours}H ${minutes}M ${seconds}S`;
-
-    if (distance < 0) {
-        clearInterval(x);
-        demoElement.innerHTML = "EXPIRED";
-    }
+  if (distance < 0) {
+    clearInterval(x);
+    demoElement.innerHTML = "EXPIRED";
+  }
 }, 1000);
